4063562a12141d5814b5cbabaaa8c43aa6a8cb85
IrisTheSnail <salwa999adlouni@gmail.com>
2022-11-12 22:53:08 +0100

    still need to figure out how to make the foreign key appear in admin

--- suivi_fact/admin.py
+++ suivi_fact/admin.py
@@ -6,7 +6,7 @@ class FournisseurAdmin(admin.ModelAdmin):
     list_display = ('fourn_id', 'nom', 'ville')
     
 class CommandeAdmin(admin.ModelAdmin):
-    list_display = ('comm_id', )
+    list_display = ('comm_id', '')
     
 class FactureAdmin(admin.ModelAdmin):
     list_display = ('fact_id', 'montant', 'tauxTVA', 'montantTTC', 'Date_arrivee', 'Date_mandat', 'Date_compta', 'num_op', 'num_fact')
@@ -17,3 +17,4 @@ admin.site.register(Commande, CommandeAdmin)
 admin.site.register(Facture, FactureAdmin)
 admin.site.register(Departement)
 admin.site.register(Mouvement)
+
--- suivi_fact/urls.py
+++ suivi_fact/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
     path('', views.home, name="first-interface-home"),
@@ -7,3 +10,7 @@ urlpatterns = [
     path('dcf/', views.dcf, name='--dcf'),
     path('st/', views.st, name='--st'),
 ]
+
+admin.site.site_header  =  "R.A.D.E.E.F."  
+admin.site.site_title  =  "Custom bookstore admin site"
+admin.site.index_title  =  "Portail admin de gestion de factures"
